7bfdbdc39d5e052bf03d032328552fba89a066cb
Okan Celik <okan@celik.tech>
2023-07-30 22:37:24 +0200

    feat: add gaming modules and update flake

--- host/pr0ject/user_packages.nix
+++ host/pr0ject/user_packages.nix
@@ -7,11 +7,12 @@ let
         (builtins.readDir path)));
   shell = getDirfolders ../../modules/shell;
   desktop = getDirfolders ../../modules/desktop;
+  games = getDirfolders ../../modules/games;
   programs = getDirfolders ../../modules/programs;
   services = getDirfolders ../../modules/services;
 in
 {
-  imports = [ ./home.nix ] ++ desktop ++ programs ++ services ++ shell;
+  imports = [ ./home.nix ] ++ desktop ++ programs ++ games ++ services ++ shell;
 
   config.modules = rec {
     desktop = {
@@ -26,6 +27,9 @@ in
       brave.enable = true;
       latex.enable = true;
       devops-tools.enable = true;
+    };
+    games = {
+      minecraft.enable = true;
     };
     services = {
       picom.enable = false;
---
+++ modules/games/minecraft/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, config, user, theme, ... }:
+let cfg = config.modules.games.minecraft;
+in with lib; {
+  options.modules.games.minecraft = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "enable minecraft tools";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home = {
+      packages = with pkgs; [
+        # MC related
+        packwiz
+        prismlauncher
+      ];
+    };
+  };
+}
